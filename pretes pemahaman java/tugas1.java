96a82fec2d563e30eeb162a2dd10ccffa7c07a67
mubaroroh <lulukmubaroroh@gmail.com>
2017-02-27 09:56:09 +0700

    Created by Coding Ground Client

---
+++ pretes pemahaman java/tugas1.java
@@ -0,0 +1,8 @@
+public class tugas1{
+    
+    public static void main(String []args){
+         String nama = "Luluk";
+         String kota = "Jepara";
+        System.out.println("nama : " + nama + "\nKota: " + kota);c 
+     }
+}
